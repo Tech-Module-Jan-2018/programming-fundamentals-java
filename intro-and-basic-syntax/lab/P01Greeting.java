6ee3bfa01c74fa0efab6faf30ce98ad925ad299a
Alen Paunov <alen.paunov@gmail.com>
2018-01-22 22:14:27 +0200

    added comments

--- intro-and-basic-syntax/lab/P01Greeting.java
+++ intro-and-basic-syntax/lab/P01Greeting.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class P01Greeting {
 
     public static void main(String[] args) {
+		// Scanner is the JAVA equivalent of console
+		// We need to import it -> see on top
+		// console is the name of the variable
         Scanner console = new Scanner(System.in);
 
         String name = console.nextLine();
--- intro-and-basic-syntax/lab/P02AddTwoNumbers.java
+++ intro-and-basic-syntax/lab/P02AddTwoNumbers.java
@@ -4,6 +4,8 @@ public class P02AddTwoNumbers {
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+		
+		// We parse the same way as in C#
         int num1 = Integer.parseInt(console.nextLine());
         int num2 = Integer.parseInt(console.nextLine());
 
