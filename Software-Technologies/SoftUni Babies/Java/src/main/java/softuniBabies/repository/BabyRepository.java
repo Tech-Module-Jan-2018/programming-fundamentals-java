c1d160e22289cd670e0344ee64cd9766d778b510
AtanasovAtanas <atanas4537@gmail.com>
2018-08-11 18:15:33 +0300

    removed softuni software technologies exam prep

--- Software-Technologies/SoftUni Babies/Java/src/main/java/softuniBabies/repository/BabyRepository.java
+++
@@ -1,7 +0,0 @@
-package softuniBabies.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import softuniBabies.entity.Baby;
-
-public interface BabyRepository extends JpaRepository<Baby, Integer> {
-}
